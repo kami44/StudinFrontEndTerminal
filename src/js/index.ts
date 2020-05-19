a4a3d7cc07f4596215fb256401903a080c40e3f8
Patrick Poul Nielsen <patr9244@edu.easj.dk>
2020-05-19 12:54:11 +0200

    terminalforside

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,4 +1,5 @@
-interface Person {
+
+/*interface Person {
     firstName: string;
     lastName: string;
 }
@@ -9,4 +10,41 @@ function greeter(person: Person): string {
 let user: Person = { firstName: "John", lastName: "Doe" };
 
 let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+element.innerHTML = greeter(user);*/
+interface IStudent{
+    id: number;
+    firstName: string;
+    lastName: string;
+    
+}
+let buttonwrap: HTMLDivElement = <HTMLDivElement> document.getElementById("buttonwrap");
+
+new Vue({
+    el: "#wrapper",
+    data:{
+        userStudent:{
+            courses: [
+                {id:1, name:"Programmering Lokale A"},
+                {id:2, name:"Technology Lokale B"},
+                {id:3, name:"Systemudvikling Lokale C"},
+            ],
+            firstName:"Lal",
+            lastName:"john",
+            currentCourse: {
+                id:1,
+                name:"Programmering Lokale A",
+                time:"Kl. 09.00",
+                
+            students: [
+                {id:1, firstName:"Patrick Poul", lastName:"Nielsen"}
+            ],
+            
+ 
+          }
+        },
+
+    }
+})
+
+
+
